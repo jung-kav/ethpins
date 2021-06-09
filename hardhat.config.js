e65b57387f3e70fbba0d2e770a56af09c54f3e8b
Kaveer Kapoor <kaveerkapoor93@gmail.com>
2021-06-08 23:24:11 -0700

    fix: add private key for ropsten deployer address

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,14 +8,19 @@ module.exports = {
   solidity: '0.8.3',
   defaultNetwork: 'hardhat',
   networks: {
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_ID}`,
+      accounts: [process.env.MAINNET_PRIVATE_KEY],
+    },
     hardhat: {
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_ID}`,
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_FORKED_MAINNET_ID}`,
         blockNumber: 12456360,
       },
     },
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_ROPSTEN_ID}`,
+      accounts: [process.env.ROPSTEN_PRIVATE_KEY],
     },
   },
 }
