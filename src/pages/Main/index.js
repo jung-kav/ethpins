1d9d22e06c11c211fff519ad61f0d8e361a0aaaf
Kaveer Kapoor <kaveerkapoor93@gmail.com>
2021-06-09 20:02:20 -0700

    fix: price oracle functionality working
    removed functionality for buy, sell and switching tokens

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,13 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useWeb3Context } from 'web3-react'
 import { ethers } from 'ethers'
+import { WETH } from '@uniswap/sdk'
 
-import { TOKEN_SYMBOLS, TOKEN_ADDRESSES, ERROR_CODES } from '../../utils'
+import { TOKEN_SYMBOLS, TOKEN_ADDRESSES } from '../../utils'
 import {
   useTokenContract,
   useExchangeContract,
   useAddressBalance,
-  useExchangeReserves,
   useExchangeAllowance,
   useTotalSupply,
 } from '../../hooks'
@@ -21,37 +21,6 @@ const GAS_MARGIN = ethers.BigNumber.from(1000)
 export function calculateGasMargin(value, margin) {
   const offset = value.mul(margin).div(ethers.BigNumber.from(10000))
   return value.add(offset)
-}
-
-// denominated in seconds
-const DEADLINE_FROM_NOW = 60 * 15
-
-// denominated in bips
-const ALLOWED_SLIPPAGE = ethers.BigNumber.from(200)
-
-function calculateSlippageBounds(value) {
-  const offset = value.mul(ALLOWED_SLIPPAGE).div(ethers.BigNumber.from(10000))
-  const minimum = value.sub(offset)
-  const maximum = value.add(offset)
-  return {
-    minimum: minimum.lt(ethers.constants.Zero) ? ethers.constants.Zero : minimum,
-    maximum: maximum.gt(ethers.constants.MaxUint256) ? ethers.constants.MaxUint256 : maximum,
-  }
-}
-
-// this mocks the getInputPrice function, and calculates the required output
-function calculateEtherTokenOutputFromInput(inputAmount, inputReserve, outputReserve) {
-  const inputAmountWithFee = inputAmount.mul(ethers.BigNumber.from(997))
-  const numerator = inputAmountWithFee.mul(outputReserve)
-  const denominator = inputReserve.mul(ethers.BigNumber.from(1000)).add(inputAmountWithFee)
-  return numerator.div(denominator)
-}
-
-// this mocks the getOutputPrice function, and calculates the required input
-function calculateEtherTokenInputFromOutput(outputAmount, inputReserve, outputReserve) {
-  const numerator = inputReserve.mul(outputAmount).mul(ethers.BigNumber.from(1000))
-  const denominator = outputReserve.sub(outputAmount).mul(ethers.BigNumber.from(997))
-  return numerator.div(denominator).add(ethers.constants.One)
 }
 
 // get exchange rate for a token/ETH pair
@@ -78,173 +47,85 @@ function getExchangeRate(inputValue, outputValue, invert = false) {
   }
 }
 
-function calculateAmount(
-  inputTokenSymbol,
-  outputTokenSymbol,
-  PINOAmount,
-  reservePINOETH,
-  reservePINOToken,
-  reserveSelectedTokenETH,
-  reserveSelectedTokenToken
-) {
-  // eth to token - buy
-  if (inputTokenSymbol === TOKEN_SYMBOLS.ETH && outputTokenSymbol === TOKEN_SYMBOLS.PINO) {
-    const amount = calculateEtherTokenInputFromOutput(PINOAmount, reservePINOETH, reservePINOToken)
-    if (amount.lte(ethers.constants.Zero) || amount.gte(ethers.constants.MaxUint256)) {
-      throw Error()
-    }
-    return amount
-  }
-
-  // token to eth - sell
-  if (inputTokenSymbol === TOKEN_SYMBOLS.PINO && outputTokenSymbol === TOKEN_SYMBOLS.ETH) {
-    const amount = calculateEtherTokenOutputFromInput(PINOAmount, reservePINOToken, reservePINOETH)
-    if (amount.lte(ethers.constants.Zero) || amount.gte(ethers.constants.MaxUint256)) {
-      throw Error()
-    }
-
-    return amount
-  }
-
-  // token to token - buy or sell
-  const buyingPINO = outputTokenSymbol === TOKEN_SYMBOLS.PINO
-
-  if (buyingPINO) {
-    // eth needed to buy x ethpins
-    const intermediateValue = calculateEtherTokenInputFromOutput(PINOAmount, reservePINOETH, reservePINOToken)
-    // calculateEtherTokenOutputFromInput
-    if (intermediateValue.lte(ethers.constants.Zero) || intermediateValue.gte(ethers.constants.MaxUint256)) {
-      throw Error()
-    }
-    // tokens needed to buy x eth
-    const amount = calculateEtherTokenInputFromOutput(
-      intermediateValue,
-      reserveSelectedTokenToken,
-      reserveSelectedTokenETH
-    )
-    if (amount.lte(ethers.constants.Zero) || amount.gte(ethers.constants.MaxUint256)) {
-      throw Error()
-    }
-    return amount
-  } else {
-    // eth gained from selling x ethpins
-    const intermediateValue = calculateEtherTokenOutputFromInput(PINOAmount, reservePINOToken, reservePINOETH)
-    if (intermediateValue.lte(ethers.constants.Zero) || intermediateValue.gte(ethers.constants.MaxUint256)) {
-      throw Error()
-    }
-    // tokens yielded from selling x eth
-    const amount = calculateEtherTokenOutputFromInput(
-      intermediateValue,
-      reserveSelectedTokenETH,
-      reserveSelectedTokenToken
-    )
-    if (amount.lte(ethers.constants.Zero) || amount.gte(ethers.constants.MaxUint256)) {
-      throw Error()
-    }
-    return amount
-  }
-}
-
 export default function Main({ stats, status }) {
   const { library, account } = useWeb3Context()
 
-  // selected token
-  const [selectedTokenSymbol, setSelectedTokenSymbol] = useState(TOKEN_SYMBOLS.ETH)
-
   // get exchange contracts
   const exchangeContractPINO = useExchangeContract(TOKEN_ADDRESSES.PINO)
-  const exchangeContractSelectedToken = useExchangeContract(TOKEN_ADDRESSES[selectedTokenSymbol])
   const exchangeContractDAI = useExchangeContract(TOKEN_ADDRESSES.DAI)
 
   // get token contracts
   const tokenContractPINO = useTokenContract(TOKEN_ADDRESSES.PINO)
-  const tokenContractSelectedToken = useTokenContract(TOKEN_ADDRESSES[selectedTokenSymbol])
+  const tokenContractETH = useTokenContract(WETH[process.env.REACT_APP_CHAIN_ID].address)
 
   // get balances
   const balanceETH = useAddressBalance(account, TOKEN_ADDRESSES.ETH)
   const balancePINO = useAddressBalance(account, TOKEN_ADDRESSES.PINO)
-  const balanceSelectedToken = useAddressBalance(account, TOKEN_ADDRESSES[selectedTokenSymbol])
 
   // totalsupply
   const totalSupply = useTotalSupply(tokenContractPINO)
 
   // get allowances
   const allowancePINO = useExchangeAllowance(account, TOKEN_ADDRESSES.PINO)
-  const allowanceSelectedToken = useExchangeAllowance(account, TOKEN_ADDRESSES[selectedTokenSymbol])
+  const allowanceETH = useExchangeAllowance(account, WETH[process.env.REACT_APP_CHAIN_ID].address)
 
   // get reserves
-  const reservePINOETH = useAddressBalance(exchangeContractPINO && exchangeContractPINO.address, TOKEN_ADDRESSES.ETH)
-  const reservePINOToken = useAddressBalance(exchangeContractPINO && exchangeContractPINO.address, TOKEN_ADDRESSES.PINO)
-  const { reserveETH: reserveSelectedTokenETH, reserveToken: reserveSelectedTokenToken } = useExchangeReserves(
-    TOKEN_ADDRESSES[selectedTokenSymbol]
+  const reserveETH = useAddressBalance(
+    exchangeContractPINO && exchangeContractPINO.address,
+    WETH[process.env.REACT_APP_CHAIN_ID].address
   )
+  const reservePINO = useAddressBalance(exchangeContractPINO && exchangeContractPINO.address, TOKEN_ADDRESSES.PINO)
 
-  const reserveDAIETH = useAddressBalance(exchangeContractDAI && exchangeContractDAI.address, TOKEN_ADDRESSES.ETH)
+  const reserveDAIETH = useAddressBalance(
+    exchangeContractDAI && exchangeContractDAI.address,
+    WETH[process.env.REACT_APP_CHAIN_ID].address
+  )
   const reserveDAIToken = useAddressBalance(exchangeContractDAI && exchangeContractDAI.address, TOKEN_ADDRESSES.DAI)
 
   const [USDExchangeRateETH, setUSDExchangeRateETH] = useState()
-  const [USDExchangeRateSelectedToken, setUSDExchangeRateSelectedToken] = useState()
 
   const ready = !!(
     (account === null || allowancePINO) &&
-    (selectedTokenSymbol === 'ETH' || account === null || allowanceSelectedToken) &&
+    (account === null || allowanceETH) &&
     (account === null || balanceETH) &&
     (account === null || balancePINO) &&
-    (account === null || balanceSelectedToken) &&
-    reservePINOETH &&
-    reservePINOToken &&
-    (selectedTokenSymbol === 'ETH' || reserveSelectedTokenETH) &&
-    (selectedTokenSymbol === 'ETH' || reserveSelectedTokenToken) &&
-    selectedTokenSymbol &&
-    (USDExchangeRateETH || USDExchangeRateSelectedToken)
+    reserveETH &&
+    reservePINO &&
+    USDExchangeRateETH
   )
 
   useEffect(() => {
     try {
       const exchangeRateDAI = getExchangeRate(reserveDAIETH, reserveDAIToken)
-
-      if (selectedTokenSymbol === TOKEN_SYMBOLS.ETH) {
-        setUSDExchangeRateETH(exchangeRateDAI)
-      } else {
-        const exchangeRateSelectedToken = getExchangeRate(reserveSelectedTokenETH, reserveSelectedTokenToken)
-        if (exchangeRateDAI && exchangeRateSelectedToken) {
-          setUSDExchangeRateSelectedToken(
-            exchangeRateDAI.mul(ethers.BigNumber.from(10).pow(ethers.BigNumber.from(18))).div(exchangeRateSelectedToken)
-          )
-        }
-      }
+      setUSDExchangeRateETH(exchangeRateDAI)
     } catch {
       setUSDExchangeRateETH()
-      setUSDExchangeRateSelectedToken()
     }
-  }, [reserveDAIETH, reserveDAIToken, reserveSelectedTokenETH, reserveSelectedTokenToken, selectedTokenSymbol])
+  }, [reserveDAIETH, reserveDAIToken])
 
   function _dollarize(amount, exchangeRate) {
     return amount.mul(exchangeRate).div(ethers.BigNumber.from(10).pow(ethers.BigNumber.from(18)))
   }
 
   function dollarize(amount) {
-    return _dollarize(
-      amount,
-      selectedTokenSymbol === TOKEN_SYMBOLS.ETH ? USDExchangeRateETH : USDExchangeRateSelectedToken
-    )
+    return _dollarize(amount, USDExchangeRateETH)
   }
 
   const [dollarPrice, setDollarPrice] = useState()
   useEffect(() => {
     try {
-      const PINOExchangeRateETH = getExchangeRate(reservePINOToken, reservePINOETH)
+      const PINOExchangeRateETH = getExchangeRate(reservePINO, reserveETH)
       setDollarPrice(
         PINOExchangeRateETH.mul(USDExchangeRateETH).div(ethers.BigNumber.from(10).pow(ethers.BigNumber.from(18)))
       )
     } catch {
       setDollarPrice()
     }
-  }, [USDExchangeRateETH, reservePINOETH, reservePINOToken])
+  }, [USDExchangeRateETH, reserveETH, reservePINO])
 
   async function unlock(buyingPINO = true) {
-    const contract = buyingPINO ? tokenContractSelectedToken : tokenContractPINO
-    const spenderAddress = buyingPINO ? exchangeContractSelectedToken.address : exchangeContractPINO.address
+    const contract = buyingPINO ? tokenContractETH : tokenContractPINO
+    const spenderAddress = exchangeContractPINO.address
 
     const estimatedGasLimit = await contract.estimateGas.approve(spenderAddress, ethers.constants.MaxUint256)
     const estimatedGasPrice = await library
@@ -255,244 +136,6 @@ export default function Main({ stats, status }) {
       gasLimit: calculateGasMargin(estimatedGasLimit, GAS_MARGIN),
       gasPrice: estimatedGasPrice,
     })
-  }
-
-  // buy functionality
-  const validateBuy = useCallback(
-    numberOfPINO => {
-      // validate passed amount
-      let parsedValue
-      try {
-        parsedValue = ethers.utils.parseUnits(numberOfPINO, 18)
-      } catch (error) {
-        error.code = ERROR_CODES.INVALID_AMOUNT
-        throw error
-      }
-
-      let requiredValueInSelectedToken
-      try {
-        requiredValueInSelectedToken = calculateAmount(
-          selectedTokenSymbol,
-          TOKEN_SYMBOLS.PINO,
-          parsedValue,
-          reservePINOETH,
-          reservePINOToken,
-          reserveSelectedTokenETH,
-          reserveSelectedTokenToken
-        )
-      } catch (error) {
-        error.code = ERROR_CODES.INVALID_TRADE
-        throw error
-      }
-
-      // get max slippage amount
-      const { maximum } = calculateSlippageBounds(requiredValueInSelectedToken)
-
-      // the following are 'non-breaking' errors that will still return the data
-      let errorAccumulator
-      // validate minimum ether balance
-      if (balanceETH && balanceETH.lt(ethers.utils.parseEther('.01'))) {
-        const error = Error()
-        error.code = ERROR_CODES.INSUFFICIENT_ETH_GAS
-        if (!errorAccumulator) {
-          errorAccumulator = error
-        }
-      }
-
-      // validate minimum selected token balance
-      if (balanceSelectedToken && maximum && balanceSelectedToken.lt(maximum)) {
-        const error = Error()
-        error.code = ERROR_CODES.INSUFFICIENT_SELECTED_TOKEN_BALANCE
-        if (!errorAccumulator) {
-          errorAccumulator = error
-        }
-      }
-
-      // validate allowance
-      if (selectedTokenSymbol !== 'ETH') {
-        if (allowanceSelectedToken && maximum && allowanceSelectedToken.lt(maximum)) {
-          const error = Error()
-          error.code = ERROR_CODES.INSUFFICIENT_ALLOWANCE
-          if (!errorAccumulator) {
-            errorAccumulator = error
-          }
-        }
-      }
-
-      return {
-        inputValue: requiredValueInSelectedToken,
-        maximumInputValue: maximum,
-        outputValue: parsedValue,
-        error: errorAccumulator,
-      }
-    },
-    [
-      allowanceSelectedToken,
-      balanceETH,
-      balanceSelectedToken,
-      reservePINOETH,
-      reservePINOToken,
-      reserveSelectedTokenETH,
-      reserveSelectedTokenToken,
-      selectedTokenSymbol,
-    ]
-  )
-
-  async function buy(maximumInputValue, outputValue) {
-    const deadline = Math.ceil(Date.now() / 1000) + DEADLINE_FROM_NOW
-
-    const estimatedGasPrice = await library
-      .getGasPrice()
-      .then(gasPrice => gasPrice.mul(ethers.BigNumber.from(150)).div(ethers.BigNumber.from(100)))
-
-    if (selectedTokenSymbol === TOKEN_SYMBOLS.ETH) {
-      const estimatedGasLimit = await exchangeContractPINO.estimateGas.ethToTokenSwapOutput(outputValue, deadline, {
-        value: maximumInputValue,
-      })
-      return exchangeContractPINO.ethToTokenSwapOutput(outputValue, deadline, {
-        value: maximumInputValue,
-        gasLimit: calculateGasMargin(estimatedGasLimit, GAS_MARGIN),
-        gasPrice: estimatedGasPrice,
-      })
-    } else {
-      const estimatedGasLimit = await exchangeContractSelectedToken.estimateGas.tokenToTokenSwapOutput(
-        outputValue,
-        maximumInputValue,
-        ethers.constants.MaxUint256,
-        deadline,
-        TOKEN_ADDRESSES.PINO
-      )
-      return exchangeContractSelectedToken.tokenToTokenSwapOutput(
-        outputValue,
-        maximumInputValue,
-        ethers.constants.MaxUint256,
-        deadline,
-        TOKEN_ADDRESSES.PINO,
-        {
-          gasLimit: calculateGasMargin(estimatedGasLimit, GAS_MARGIN),
-          gasPrice: estimatedGasPrice,
-        }
-      )
-    }
-  }
-
-  // sell functionality
-  const validateSell = useCallback(
-    numberOfPINO => {
-      // validate passed amount
-      let parsedValue
-      try {
-        parsedValue = ethers.utils.parseUnits(numberOfPINO, 18)
-      } catch (error) {
-        error.code = ERROR_CODES.INVALID_AMOUNT
-        throw error
-      }
-
-      // how much ETH or tokens the sale will result in
-      let requiredValueInSelectedToken
-      try {
-        requiredValueInSelectedToken = calculateAmount(
-          TOKEN_SYMBOLS.PINO,
-          selectedTokenSymbol,
-          parsedValue,
-          reservePINOETH,
-          reservePINOToken,
-          reserveSelectedTokenETH,
-          reserveSelectedTokenToken
-        )
-      } catch (error) {
-        error.code = ERROR_CODES.INVALID_EXCHANGE
-        throw error
-      }
-
-      // slippage-ized
-      const { minimum } = calculateSlippageBounds(requiredValueInSelectedToken)
-
-      // the following are 'non-breaking' errors that will still return the data
-      let errorAccumulator
-      // validate minimum ether balance
-      if (balanceETH.lt(ethers.utils.parseEther('.01'))) {
-        const error = Error()
-        error.code = ERROR_CODES.INSUFFICIENT_ETH_GAS
-        if (!errorAccumulator) {
-          errorAccumulator = error
-        }
-      }
-
-      // validate minimum ethpins balance
-      if (balancePINO.lt(parsedValue)) {
-        const error = Error()
-        error.code = ERROR_CODES.INSUFFICIENT_SELECTED_TOKEN_BALANCE
-        if (!errorAccumulator) {
-          errorAccumulator = error
-        }
-      }
-
-      // validate allowance
-      if (allowancePINO.lt(parsedValue)) {
-        const error = Error()
-        error.code = ERROR_CODES.INSUFFICIENT_ALLOWANCE
-        if (!errorAccumulator) {
-          errorAccumulator = error
-        }
-      }
-
-      return {
-        inputValue: parsedValue,
-        outputValue: requiredValueInSelectedToken,
-        minimumOutputValue: minimum,
-        error: errorAccumulator,
-      }
-    },
-    [
-      allowancePINO,
-      balanceETH,
-      balancePINO,
-      reservePINOETH,
-      reservePINOToken,
-      reserveSelectedTokenETH,
-      reserveSelectedTokenToken,
-      selectedTokenSymbol,
-    ]
-  )
-
-  async function sell(inputValue, minimumOutputValue) {
-    const deadline = Math.ceil(Date.now() / 1000) + DEADLINE_FROM_NOW
-
-    const estimatedGasPrice = await library
-      .getGasPrice()
-      .then(gasPrice => gasPrice.mul(ethers.BigNumber.from(150)).div(ethers.BigNumber.from(100)))
-
-    if (selectedTokenSymbol === TOKEN_SYMBOLS.ETH) {
-      const estimatedGasLimit = await exchangeContractPINO.estimateGas.tokenToEthSwapInput(
-        inputValue,
-        minimumOutputValue,
-        deadline
-      )
-      return exchangeContractPINO.tokenToEthSwapInput(inputValue, minimumOutputValue, deadline, {
-        gasLimit: calculateGasMargin(estimatedGasLimit, GAS_MARGIN),
-        gasPrice: estimatedGasPrice,
-      })
-    } else {
-      const estimatedGasLimit = await exchangeContractPINO.estimateGas.tokenToTokenSwapInput(
-        inputValue,
-        minimumOutputValue,
-        ethers.constants.One,
-        deadline,
-        TOKEN_ADDRESSES[selectedTokenSymbol]
-      )
-      return exchangeContractPINO.tokenToTokenSwapInput(
-        inputValue,
-        minimumOutputValue,
-        ethers.constants.One,
-        deadline,
-        TOKEN_ADDRESSES[selectedTokenSymbol],
-        {
-          gasLimit: calculateGasMargin(estimatedGasLimit, GAS_MARGIN),
-          gasPrice: estimatedGasPrice,
-        }
-      )
-    }
   }
 
   async function burn(amount) {
@@ -511,24 +154,24 @@ export default function Main({ stats, status }) {
   }
 
   return stats ? (
-    <Stats reservePINOToken={reservePINOToken} totalSupply={totalSupply} ready={ready} balancePINO={balancePINO} />
+    <Stats reservePINOToken={reservePINO} totalSupply={totalSupply} ready={ready} balancePINO={balancePINO} />
   ) : status ? (
     <Status totalSupply={totalSupply} ready={ready} balancePINO={balancePINO} />
   ) : (
     <Body
-      selectedTokenSymbol={selectedTokenSymbol}
-      setSelectedTokenSymbol={setSelectedTokenSymbol}
+      selectedTokenSymbol={TOKEN_SYMBOLS.ETH}
+      setSelectedTokenSymbol={() => {}}
       ready={ready}
       unlock={unlock}
-      validateBuy={validateBuy}
-      buy={buy}
-      validateSell={validateSell}
-      sell={sell}
+      validateBuy={() => {}}
+      buy={() => {}}
+      validateSell={() => {}}
+      sell={() => {}}
       burn={burn}
       dollarize={dollarize}
       dollarPrice={dollarPrice}
       balancePINO={balancePINO}
-      reservePINOToken={reservePINOToken}
+      reservePINOToken={reservePINO}
       totalSupply={totalSupply}
     />
   )
